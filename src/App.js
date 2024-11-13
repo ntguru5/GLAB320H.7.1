adabd753994be8ff86871e2c66f518f267a2d86a
Ken Yeh <yehken@gmail.com>
2024-11-13 10:14:47 -0800

    start lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Import our components
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  // Constant with your API Key
+  const apiKey = "97fe76d0";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async(searchTerm) => {
+    // Make fetch request and store the response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a JavaScript object
+    const data = await response.json();
+    // Set the Movie state to the received data
+    setMovie(data);
+  };
+
+  // We pass the getMovie function as a prop called moviesearch
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form moviesearch={getMovie} />
+      <MovieDisplay />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+export default function Form(props){
+  // The component must return some JSX
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+}
